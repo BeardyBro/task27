24045643ce75c8146b207bf5cf748ef707e3478a
Andrey Vershinsky <vershinsky90@gmail.com>
2023-06-18 20:27:29 +0300

    task27

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int number = ReadLine();
+int ReadLine()
+{
+Console.WriteLine("Input a number");
+string input = Console.ReadLine();
+int result = Convert.ToInt32(input);
+return result;
+}
+int Sum(int number)
+{
+int result=0;
+while (number>0)
+{
+    result +=number%10;
+    number = number/10;
+}
+return result;
+}
+Console.WriteLine($"Sum of all digits in {number} = {Sum(number)}");
